216fe0e3d4f65f263ca2d938024c223ca0d59a0c
Aubrey Koski <aubkos@Aubreys-MacBook-Pro.local>
2019-11-23 15:03:42 -0600

    update app.js
    added reqs and prompts

--- test/app.js
+++ test/app.js
@@ -1,2 +1,55 @@
 const inquirer = require("inquirer");
-//require engineer, employee//
+const fs = require("fs");
+const util = require("util");
+const employee = require("./lib/Employee");
+const intern = require("./lib/Intern");
+const engineer = require("./lib/Engineer");
+const manager = require("./lib/Manager");
+
+
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Enter employee's name"
+    },
+    {
+      type: "input",
+      name: "role",
+      message: "Enter employee's role"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter employee's GitHub Username"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter employee's email?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Enter employee's id?"
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Enter employee's school?"
+      },
+  ]);
+}
+//gen html for required use cards//
+function addManager(){
+    
+}
+
+
+
+//get info for each employee subclass and saves intern and engineer info to an array async function
+
