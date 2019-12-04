7d04a89086e1c151d8a6f8150d842934ffcb27be
Aubrey Koski <aubkos@Aubreys-MacBook-Pro.local>
2019-12-04 17:07:48 -0600

    final updates

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,20 +1,30 @@
 class Employee {
-    constructor(name, id, email){
+    constructor(name, id, email) {
         this.name = name;
         this.id = id;
         this.email = email;
     }
-    getName(){
-        return this.name
+
+    getName() {
+        return this.name;
+        console.log(`This vehicle has ${this.name} wheels`);
+        console.log(`This vehicle has an id of ${this.id}`);
     }
-    getId(){
-        return this.id
+    getId() {
+        return this.id;
+        console.log(`This vehicle has ${this.id} wheels`);
+        console.log(`This vehicle has an id of ${this.id}`);
     }
-    getEmail(){
-        return this.email
+    getEmail() {
+        return this.email;
+        console.log(`This vehicle has ${this.email} wheels`);
+        console.log(`This vehicle has an id of ${this.id}`);
     }
-    getRole(){
-        return "employee"
+    getRole() {
+        return "Employee";
+        console.log(`This vehicle has ${this.numberOfWheels} wheels`);
+        console.log(`This vehicle has an id of ${this.id}`);
     }
+
 }
 module.exports = Employee;
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # htmlgen
-html generator
+DeveloperProfileGenerator
+Project Title
+DeveloperProfileGenerator - This project covers a generation of a team webpage which displays team's basic info which can accessed to get team members emails and GitHub profiles.
+
+Built With
+Html - UI features
+CSS - Style sheet
+Node JS
+Javascript
+
+Versioning
+Github
+
+Screenshots
